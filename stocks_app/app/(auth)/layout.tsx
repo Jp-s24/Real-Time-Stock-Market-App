7c6dc4ba4c86bcedc20a8d4ac5b46966ec41d04c
Jp <jeanpaul.saliba@ontariotechu.net>
2026-08-03 21:19:03 -0400

    updated layout and auth

---
+++ stocks_app/app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header"
+import React from "react"
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <main className="min-h-screen text-gray-400">
+
+        </main>
+    )
+}
+
+export default Layout
